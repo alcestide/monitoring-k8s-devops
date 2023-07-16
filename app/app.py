b549a644ab098441d6248df3617469427e7e04ea
Angelo Panariti <angpanariti@gmail.com>
2023-07-16 10:49:23 +0200

    Dockerfile

--- app/app.py
+++ app/app.py
@@ -8,10 +8,15 @@ app = Flask(__name__)
 def index():
     cpu_percent = psutil.cpu_percent()
     mem_percent = psutil.virtual_memory().percent
+    disk_percent = psutil.disk_usage('/').percent
     Message = None
     if cpu_percent > 80 or mem_percent > 80:
         Message = "Detected high CPU/Memory usage."
-    return f'CPU: {cpu_percent}%\nMem: {mem_percent}%\n{Message}'
+    return render_template('index.html',
+                           cpu_percent=cpu_percent,
+                           mem_percent=mem_percent,
+                           disk_percent=disk_percent,
+                           message=Message)
 
 
 if __name__ == '__main__':
